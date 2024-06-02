09d03e692747a0528fffe148f249e3ace96e4a43
Vishesh Rajput <156611608+ivisheshrajput@users.noreply.github.com>
2024-06-03 02:38:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+// app.listen(3001, () => {
+//   console.log("App is running succesfully");
+// });
+
+//load config from env file
+require("dotenv").config();
+const PORT = process.env.PORT || 3700;
+
+//middleware to parse json request body
+app.use(express.json());
+
+//import routes for TODO API
+const todoRoutes = require("./routes/TodoRoutes");
+//mount the todo APIs routes
+app.use("/api/v1", todoRoutes);
+
+//start server
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
+
+//connect to database
+const dbConnect = require("./config/database");
+dbConnect();
+
+app.get("/", (req, res) => {
+  res.send(`<h1>TODO BACKEND ROUTE CHECK</h1>`);
+});
